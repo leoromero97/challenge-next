f5912a0d5f010b288be839dee3b748d98be462ee
leoromero97 <leonardogerbacio@gmail.com>
2021-08-30 16:54:33 -0300

    Solved fixe order import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
